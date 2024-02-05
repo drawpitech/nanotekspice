edcd0b75b27f0ffb8f689dc59583e8a24c5d3ef9
celian <celian.lombardot@epitech.eu>
2024-02-05 10:10:04 +0100

    Add Tristate and Icomponent given in subject

--- include/tekspice.hpp
+++ include/tekspice.hpp
@@ -5,8 +5,26 @@
 ** tekspice
 */
 
+#pragma once
+
+#include <string>
+
 namespace nts {
 
-int tekspice(int argc, char **argv);
+    int tekspice(int argc, char **argv);
 
-}  // namespace nts
+    enum Tristate {
+        Undefined = (-true),
+        True = true,
+        False = false
+    };
+
+    class IComponent
+    {
+        public:
+        virtual ~IComponent() = default;
+        virtual void simulate(std::size_t tick) = 0;
+        virtual nts::Tristate compute (std::size_t pin) = 0;
+        virtual void setLink (std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
+    };
+}
