811f76a485beb58ef90e3bf459bb6241a5af6144
Clément <clement2104.boillot@gmail.com>
2024-02-05 09:05:25 +0100

    Init project

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cc = pkgs.gcc12;
+
+        deps = with pkgs; [
+          libcxx
+          gnumake
+          criterion
+        ] ++ [ cc ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            unzip
+            gcovr
+            ltrace
+            valgrind
+            python311Packages.compiledb
+            man-pages
+            man-pages-posix
+            gdb
+          ] ++ deps;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
