81423c81eeac6bcd81d7705e56362da5a29f9f02
Clément <clement2104.boillot@gmail.com>
2024-02-14 23:43:12 +0100

    Add last part of parsing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -70,8 +70,8 @@ void nts::Parser::addChipset(const std::string &line, Circuit &circuit)
     // Validate chipset line with regex
     // example:
     //     4001 super_chip
-    const std::regex r{R"(^[a-zA-Z0-9]+ [a-zA-Z0-9_-]+$)"};
-    if (!std::regex_search(line, r))
+    if (!std::regex_search(
+            line, std::regex{R"(^[a-zA-Z0-9]+ [a-zA-Z0-9_-]+$)"}))
         throw std::invalid_argument("Invalid chipset line");
 
     std::string chipset = line.substr(0, line.find(' '));
@@ -103,4 +103,24 @@ void nts::Parser::addLink(const std::string &line, Circuit &circuit)
     dummy.AddComponent(&Orgate);
     dummy.AddComponent(&out_b);
     */
+
+    // Validate link line with regex
+    // example:
+    //     in_1:1 and_gate:1
+    if (!std::regex_search(
+            line,
+            std::regex{R"(^[a-zA-Z0-9-_]+:[0-9]+ [a-zA-Z0-9-_]+:[0-9]+$)"}))
+        throw std::invalid_argument("Invalid chipset line");
+
+    std::string part1 = line.substr(0, line.find(' '));
+    std::string part2 = line.substr(part1.size() + 1);
+
+    IComponent &comp1 = circuit.getComponent(part1.substr(0, part1.find(':')));
+    IComponent &comp2 = circuit.getComponent(part2.substr(0, part2.find(':')));
+
+    size_t pin1 = std::stoi(part1.substr(part1.find(':') + 1));
+    size_t pin2 = std::stoi(part2.substr(part2.find(':') + 1));
+
+    comp1.setLink(pin1, comp2, pin2);
+    comp2.setLink(pin2, comp1, pin1);
 }
